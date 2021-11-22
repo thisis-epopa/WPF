ba03da9cfdfc72fc628f6ead244170a20ddba67e
thisis-epopa <re0087xv@gmail.com>
2021-11-22 11:48:31 +0900

    This is the first commit

---
+++ WpfApp1/WpfApp1/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    using WpfApp1.Views;
+    using WpfApp1.ViewModels;
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ///<summary>
+            ///Create an instance of View/MainView
+            /// </summary>
+            var w = new MainView();
+
+            ///<summary>
+            ///Create an instance of ViewModels/MainViewModel
+            /// </summary>
+            
+            var vm = new MainViewModel();
+
+            /// <summary>
+            /// Set an instance of MainViewModel as that of Mainview
+            /// </summary>
+            
+            w.DataContext = vm;
+
+            /// <summary>
+            /// Show the MainView;
+            /// </summary>
+            w.Show();
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/ViewModels/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1.ViewModels
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    /// <summary>
+    /// The Class below has the effect of DataContext to MainView.xaml
+    /// And this class implements INotifyPropertyChanged to support one-way and two-way bindings
+    /// Such that UI elements updates when the sorce has been changed dynamically
+    /// </summary>
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        private string _inputString;
+        private string _upperString;
+        /// <summary>
+        /// Declare the event
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string InputString
+        {
+            get { return this._inputString; }
+            set
+            {
+                if(this._inputString != value)
+                {
+                    this._inputString = value;
+                    this.UpperString = this._inputString.ToUpper();
+                    OnPropertyChanged();
+                    System.Diagnostics.Debug.Write("UpperString=" + this.UpperString);
+                }
+            }
+        }
+
+        public string UpperString
+        {
+            get { return this._upperString; }
+            private set
+            {
+                if(this._upperString != value)
+                {
+                    this._upperString = value;
+                    OnPropertyChanged();
+                }
+                   
+            }
+        }
+
+        /// <summary>
+        /// Create the OnPropertyChanged method to raise the event 
+        ///
+        /// </summary>
+        /// <param name="name"> the calling member's name will be used as parameter</param>
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
